49c1c9b0f375c9beea364b38e8b070a63264da78
luke-youngmin-cho <makebaram@gmail.com>
2025-09-16 12:45:28 +0900

    다익스트라

--- 20250806/28일차.cs
+++ 20250806/28일차.cs
@@ -67,70 +67,156 @@
 //    }
 //}
 
+//class Program
+//{
+//    static void Main()
+//    {
+//        int[] deltaY = { -1, 0, 1, 0 };
+//        int[] deltaX = { 0, -1, 0, 1 };
+
+//        int n, m;
+//        int[,] map;
+//        bool[,] visited;
+//        int[,] distance;
+
+//        string[] input = Console.ReadLine().Split();
+//        n = int.Parse(input[0]);
+//        m = int.Parse(input[1]);
+
+//        map = new int[n, m];
+//        visited = new bool[n, m];
+//        distance = new int[n, m];
+
+//        for (int i = 0; i < n; i++)
+//        {
+//            string line = Console.ReadLine();
+//            for (int j = 0; j < m; j++)
+//            {
+//                map[i, j] = line[j] - '0';
+//            }
+//        }
+
+//        // 예약 목록
+//        Queue<(int, int)> q = new Queue<(int, int)> ();
+//        visited[0, 0] = true;
+//        distance[0, 0] = 1;
+//        q.Enqueue((0, 0));
+
+//        // 예약 한거 꺼내오기
+//        while (q.Count > 0)
+//        {
+//            // 오래기다린놈 끄집어오기
+//            (int y, int x) = q.Dequeue();
+//            for (int i = 0; i < 4; i++)
+//            {
+//                int nextY = y + deltaY[i];
+//                int nextX = x + deltaX[i];
+
+//                // 범위초과 확인
+//                if (nextY < 0 || nextY >= n || nextX < 0 || nextX >= m)
+//                    continue;
+//                // 연결되어있는지 확인
+//                if (map[nextY, nextX] == 0)
+//                    continue;
+//                // 이미방문했는지확인
+//                if (visited[nextY, nextX] == true)
+//                    continue;
+
+//                // 예약하기
+//                q.Enqueue((nextY, nextX));
+
+//                // 방문처리
+//                visited[nextY, nextX] = true;
+//                distance[nextY, nextX] = distance[y, x] + 1;
+//            }
+//        }
+
+//        Console.WriteLine(distance[n - 1, m - 1]);
+//    }
+//}
+
+class Graph
+{
+    int[,] map = new int[,]
+        {
+            { -1, 10, -1, 18, -1, -1, -1, -1 },
+            { 10, -1, 05, 06, -1, -1, -1, -1 },
+            { -1, 05, -1, -1, -1, -1, -1, -1 },
+            { 18, 06, -1, -1, 13, -1, -1, -1 },
+            { -1, -1, -1, 13, -1, 14, 08, -1 },
+            { -1, -1, -1, -1, 14, -1, -1, 17 },
+            { -1, -1, -1, -1, 08, -1, -1, 26 },
+            { -1, -1, -1, -1, -1, 17, 26, -1 },
+        };
+
+    bool[] visited = new bool[8];
+    int[] distance = new int[8];
+    int[] parent = new int[8];
+
+    public void Dijkstra(int start)
+    {
+        Array.Fill(distance, Int32.MaxValue);
+        distance[start] = 0;
+        parent[start] = start;
+
+        while (true)
+        {
+            // 제일 좋은 후보 찾기 (최단 거리 후보)
+
+            // 가장 유력한 후보의 거리와 번호를 저장
+            int closet = Int32.MaxValue;
+            int now = -1;
+
+            for (int i = 0; i < 8; i++)
+            {
+                // 이미 방문한 정점은 스킵
+                if (visited[i] == true)
+                    continue;
+
+                // 아직 발견된 적이 없거나, 기존 후보보다 멀면 스킵
+                if (distance[i] == Int32.MaxValue || distance[i] >= closet)
+                    continue;
+
+                // 발겨한 후보중 가장 좋은 후보를 찾음, 정보를 갱신
+                closet = distance[i];
+                now = i;
+            }
+
+            if (now == -1)
+                break;
+
+            // 제일 좋은 후보 방문
+            visited[now] = true;
+
+            // 방문한 정점의 인접점 확인해서 최단거리를 갱신하다.
+            for(int next = 0; next < 8; next++)
+            {
+                // 연결이 되어있지 않으면 스킵
+                if (map[now, next] == -1)
+                    continue;
+                // 이미 방문한 정점도 스킵
+                if (visited[next] == true)
+                    continue;
+
+                // 새로 조사된 인접 정점의 최단거리 계산
+                int nextDist = distance[now] + map[now, next]; 
+                // 위의 인접 정점의 최단거리를 갱신
+                if (nextDist < distance[next])
+                {
+                    distance[next] = nextDist;
+                    parent[next] = now;
+                }
+                    
+            }
+        }
+    }
+}
+
 class Program
 {
     static void Main()
     {
-        int[] deltaY = { -1, 0, 1, 0 };
-        int[] deltaX = { 0, -1, 0, 1 };
-
-        int n, m;
-        int[,] map;
-        bool[,] visited;
-        int[,] distance;
-
-        string[] input = Console.ReadLine().Split();
-        n = int.Parse(input[0]);
-        m = int.Parse(input[1]);
-
-        map = new int[n, m];
-        visited = new bool[n, m];
-        distance = new int[n, m];
-
-        for (int i = 0; i < n; i++)
-        {
-            string line = Console.ReadLine();
-            for (int j = 0; j < m; j++)
-            {
-                map[i, j] = line[j] - '0';
-            }
-        }
-
-        // 예약 목록
-        Queue<(int, int)> q = new Queue<(int, int)> ();
-        visited[0, 0] = true;
-        distance[0, 0] = 1;
-        q.Enqueue((0, 0));
-
-        // 예약 한거 꺼내오기
-        while (q.Count > 0)
-        {
-            // 오래기다린놈 끄집어오기
-            (int y, int x) = q.Dequeue();
-            for (int i = 0; i < 4; i++)
-            {
-                int nextY = y + deltaY[i];
-                int nextX = x + deltaX[i];
-
-                // 범위초과 확인
-                if (nextY < 0 || nextY >= n || nextX < 0 || nextX >= m)
-                    continue;
-                // 연결되어있는지 확인
-                if (map[nextY, nextX] == 0)
-                    continue;
-                // 이미방문했는지확인
-                if (visited[nextY, nextX] == true)
-                    continue;
-
-                // 예약하기
-                q.Enqueue((nextY, nextX));
-
-                // 방문처리
-                visited[nextY, nextX] = true;
-                distance[nextY, nextX] = distance[y, x] + 1;
-            }
-        }
-
-        Console.WriteLine(distance[n - 1, m - 1]);
+        Graph graph = new Graph();
+        graph.Dijkstra(0);
     }
 }
